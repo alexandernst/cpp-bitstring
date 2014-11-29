f984e5824d75266f3b107f2db3c866e359221d1c
Alexander Nestorov <alexandernst@gmail.com>
2014-11-29 19:55:12 +0100

    Update docs

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cpp_2dbitstring_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2falexandernst_2fcpp_2dbitstring_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2falexandernst_2fcpp_2dbitstring_29',['cpp-bitstring [![Build Status](https://travis-ci.org/alexandernst/cpp-bitstring.svg?branch=master)](https://travis-ci.org/alexandernst/cpp-bitstring)',['../md__home_alexandernst__proyectos_cpp-bitstring__r_e_a_d_m_e.html',1,'']]]
+  ['cpp_20bitstring',['CPP Bitstring',['../md__home_alexandernst__proyectos_cpp-bitstring__r_e_a_d_m_e.html',1,'']]]
 ];
